744bdf0af14338ee64da2df11491c07aa31ba493
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-06 22:30:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/expressserver/main.go
@@ -0,0 +1,23 @@
+// cmd/expressserver/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"expressserver/internal/expressserver"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := expressserver.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
